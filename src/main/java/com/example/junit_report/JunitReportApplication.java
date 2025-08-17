6ee1c3423a86e76a4e415ab06976774cd2e6597d
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2025-08-17 21:13:13 +0200

    Initial commit

---
+++ src/main/java/com/example/junit_report/JunitReportApplication.java
@@ -0,0 +1,13 @@
+package com.example.junit_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/junit_report/JunitReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junit_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
